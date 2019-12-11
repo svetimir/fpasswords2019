8f67b575178ee1801c78f7506218fbf7e7176f8e
Georgy Yashin <ifi@yandex.ru>
2019-12-11 09:46:18 +0300

    Main commit, src version 0.2
    Only in Russian.
    Генератор паролей на питоне.
    Не требует ничего кроме встроенных библиотек, 
    генерирует сложный десятисимвольный пароль с заглавными и прописными буквами а также с цифрами.

---
+++ FluxoidPasswords2019.py
@@ -0,0 +1,36 @@
+#Генератор паролей FluxoidPasswords2019
+
+#Решаем задачу генерации 10-символьного пароля
+
+#Georgy Yashin, ifi@yandex.ru
+#10.12.2019
+#VERSION: 0.2
+
+import random
+
+#словари букв
+Letters=['a','b','c','d','e','f','g','h','i',
+         'j','k','l','m','n','o','p','q','r',
+         's','t','u','v','w','x','y','z']
+Letters2=['A','B','C','D','E','F','G','H','I',
+          'J','K','L','M','N','O','P','Q','R',
+          'S','T','U','V','W','X','Y','Z']
+
+#генерируем массив из 10 псевдослучайных чисел
+v=[str(random.randint(0,9)) for i in range(10)]
+
+#заменяем 5 случайных позиций в массиве v на буквы из словарей
+a=1
+for i in range(5):
+    j=random.randint(0,9)
+    k=random.randint(0,len(Letters)-1)
+    if random.randint(0,1) == 0:
+        v[j]=str(Letters[k])
+        a==a #дебаг
+    else:
+        a==a
+        v[j]=str(Letters2[k])
+    
+#гарантированная буква из верхнего регистра
+v[random.randint(0,9)]=str(Letters2[random.randint(0,len(Letters)-1)])
+print(''.join(v))
